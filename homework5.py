f308ebabec9e993a670722d3bc7f83a3e63c3131
yavastudent <yavastudent@gmail.com>
2024-08-01 17:50:44 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var = 12, 13, 14, 'a', 'b', 'c'
+print(immutable_var)
+#immutable_var.remove('a') # кортеж  неизменяемая упорядоченная коллекция
+mutable_list = ['man', 'women', 'children', 'old']
+print(mutable_list)
+mutable_list.remove('old')
+print(mutable_list)
